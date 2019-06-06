9e1b1e048d734a2d63f75c04d24fa1c458dcdd77
mlkersten <martin.kersten@cwi.nl>
2019-06-06 15:33:31 +0200

    Update the setup.sh script

--- data/load.sql
+++
@@ -1,4 +0,0 @@
-cat data/ONEKTUP.csv | mclient -d wisconsin -s "copy into ONEKTUP from STDIN";
-cat data/TENKTUP1.csv | mclient -d wisconsin -s "copy into TENKTUP1 from STDIN";
-cat data/TENKTUP2.csv | mclient -d wisconsin -s "copy into TENKTUP2 from STDIN";
-
